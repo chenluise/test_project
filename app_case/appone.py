b4d4bf57f33784199ff2d517db7df09ccb4a6cbe
chenluise <chenlu11011@sina.com>
2018-10-23 23:05:17 +0800

    20181023-02

--- app_case/appone.py
+++ app_case/appone.py
@@ -1,6 +1,10 @@
 #coding=utf-8
 
 from appium import webdriver
+from appium.webdriver.common.touch_action import TouchAction
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import NoSuchElementException
+from time import sleep
 
 desired_caps = {
     'platformName': 'Android',
@@ -15,3 +19,16 @@ desired_caps = {
 }
 
 driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_caps)
+
+sleep(5)
+TouchAction(driver).tap(x=444, y=49).perform()
+sleep(5)
+TouchAction(driver).tap(x=89, y=108).perform()
+sleep(5)
+TouchAction(driver).tap(x=177, y=764).perform()
+sleep(5)
+TouchAction(driver).tap(x=146, y=63).perform()
+sleep(5)
+TouchAction(driver).tap(x=144, y=50).perform()
+sleep(5)
+TouchAction(driver).tap(x=94, y=156).perform()
