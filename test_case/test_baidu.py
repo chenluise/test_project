c4bfcf768ad0056bd11dfc603850fb504024bea6
chenluise <chenlu11011@sina.com>
2019-05-23 00:16:30 +0800

    20190522-2

---
+++ test_case/test_baidu.py
@@ -0,0 +1,27 @@
+# coding=utf-8
+from selenium import webdriver
+import  unittest
+import time
+
+class BaiduTest(unittest.TestCase):
+    def setUp(self):
+        self.driver=webdriver.Chrome()
+        self.driver.implicitly_wait(30)
+        self.base_url="https://www.baidu.com"
+
+    def test_baidu(self):
+        u'''百度搜索'''
+        driver = self.driver
+        driver.get(self.base_url+"/")
+        driver.find_element_by_id("kw").clear()
+        driver.find_element_by_id("kw").send_keys("unittest")
+        driver.find_element_by_id("su").click()
+        time.sleep(3)
+        title = driver.title
+        self.assertEqual(title, U"unittest_百度搜索")
+
+    def tearDown(self):
+        self.driver.quit()
+
+if __name__ == "__main__":
+    unittest.main()
